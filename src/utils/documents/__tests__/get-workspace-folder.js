b0afe25d4866f92fb0b8cd42810a43ae823dc70c
Adaline Valentina Simonian <adalinesimonian@gmail.com>
2021-10-20 14:04:48 -0700

    fix: use first workspace for untitled documents
    Same as eslint

--- src/utils/documents/__tests__/get-workspace-folder.js
+++ src/utils/documents/__tests__/get-workspace-folder.js
@@ -8,7 +8,10 @@ mockPath.__mockPlatform('posix');
 
 jest.mock('vscode-uri', () => ({
 	URI: {
-		parse: jest.fn((/** @type {string} */ str) => ({ fsPath: str })),
+		parse: jest.fn((/** @type {string} */ str) => ({
+			fsPath: str,
+			scheme: str.startsWith('untitled') ? 'untitled' : 'file',
+		})),
 	},
 }));
 
@@ -35,30 +38,48 @@ const createMockConnection = (workspaceFolders) => {
 const createMockTextDocument = (uri) => /** @type {any} */ ({ uri });
 
 describe('getWorkspaceFolder', () => {
-	test("should return a document's workspace folder when one is applicable", async () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	test("when one is applicable, should return a document's workspace folder", async () => {
 		const connection = createMockConnection(['/home/user/directory', '/home/user/project']);
 		const document = createMockTextDocument('/home/user/project/file.js');
 
 		expect(await getWorkspaceFolder(connection, document)).toBe('/home/user/project');
 	});
 
-	test('should return undefined when no workspace folder is applicable', async () => {
+	test('when no workspace folder is applicable, should return undefined', async () => {
 		const connection = createMockConnection(['/home/user/directory']);
 		const document = createMockTextDocument('/home/user/file.js');
 
 		expect(await getWorkspaceFolder(connection, document)).toBeUndefined();
 	});
 
-	test('should return undefined when no workspace folders are returned by the connection', async () => {
+	test('when no workspace folders are returned by the connection, should return undefined', async () => {
 		const connection = createMockConnection();
 		const document = createMockTextDocument('/home/user/file.js');
 
 		expect(await getWorkspaceFolder(connection, document)).toBeUndefined();
 	});
 
-	test('should return undefined when the document has no FS path', async () => {
+	test('when the document has no FS path, should return undefined', async () => {
 		const connection = createMockConnection(['/home/user/project']);
 		const document = createMockTextDocument('');
+
+		expect(await getWorkspaceFolder(connection, document)).toBeUndefined();
+	});
+
+	test('when given an untitled document and a workspace is open, should return the first workspace folder', async () => {
+		const connection = createMockConnection(['/home/user/directory', '/home/user/project']);
+		const document = createMockTextDocument('untitled:Untitled-1');
+
+		expect(await getWorkspaceFolder(connection, document)).toBe('/home/user/directory');
+	});
+
+	test('when given an untitled document and no workspace is open, should return undefined', async () => {
+		const connection = createMockConnection();
+		const document = createMockTextDocument('untitled:Untitled-1');
 
 		expect(await getWorkspaceFolder(connection, document)).toBeUndefined();
 	});
--- src/utils/documents/get-workspace-folder.js
+++ src/utils/documents/get-workspace-folder.js
@@ -4,23 +4,30 @@ const pathIsInside = require('path-is-inside');
 const { URI } = require('vscode-uri');
 
 /**
- * Gets the workspace folder for a given document.
+ * Gets the workspace folder for a given document. If the document is an
+ * untitled file, then the first open workspace folder is returned.
  * @param {lsp.Connection} connection The language server connection to use to
  * get available workspace folders.
  * @param {lsp.TextDocument} document The document to get the workspace folder for.
  * @returns {Promise<string | undefined>}
  */
 async function getWorkspaceFolder(connection, document) {
-	const documentPath = URI.parse(document.uri).fsPath;
+	const { scheme, fsPath } = URI.parse(document.uri);
 
-	if (documentPath) {
+	if (scheme === 'untitled') {
+		const uri = (await connection.workspace.getWorkspaceFolders())?.[0]?.uri;
+
+		return uri ? URI.parse(uri).fsPath : undefined;
+	}
+
+	if (fsPath) {
 		const workspaceFolders = await connection.workspace.getWorkspaceFolders();
 
 		if (workspaceFolders) {
 			for (const { uri } of workspaceFolders) {
 				const workspacePath = URI.parse(uri).fsPath;
 
-				if (pathIsInside(documentPath, workspacePath)) {
+				if (pathIsInside(fsPath, workspacePath)) {
 					return workspacePath;
 				}
 			}
