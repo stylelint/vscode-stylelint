04303efacca831c47c4578003a62b02d608f591e
Adaline Valentina Simonian <adalinesimonian@gmail.com>
2021-10-21 16:17:00 -0700

    refactor: split up tsconfig per context
    src, scripts, and tests use separate configs now

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ const { engines } = require('./package.json');
 
 // Get only the major and minor version
 // e.g. ">=14.16.0" => "14.16"
-const nodeVersion = engines.node.match(/(\d+\.\d+)(?:\.\d+)?/)[1];
+const nodeVersion = engines.node.match(/(\d+\.\d+)(?:\.\d+)?/)?.[1];
 
 /** @type {babel.TransformOptions} */
 const config = {
