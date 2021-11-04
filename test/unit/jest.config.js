ffb5eaf37801138bacae1de32745a981b1acc3b8
Adaline Valentina Simonian <adalinesimonian@gmail.com>
2021-11-04 12:17:15 -0700

    test: require 100% unit test coverage

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -6,6 +6,15 @@ const config = {
 	testMatch: ['<rootDir>/src/**/__tests__/**/*.[jt]s?(x)'],
 	verbose: true,
 	modulePathIgnorePatterns: ['<rootDir>/.vscode-test', '<rootDir>/test/e2e/yarn-[^/]+/stylelint'],
+	collectCoverage: true,
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
 };
 
 module.exports = config;
