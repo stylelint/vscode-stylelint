112fc95f95ca4cc220253f2060e94135532eec7d
Adaline Valentina Simonian <adalinesimonian@gmail.com>
2021-11-08 12:03:38 -0800

    refactor: move vscode runner config into test/e2e

--- test/e2e/jest-runner-vscode.config.js
+++ test/e2e/jest-runner-vscode.config.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const path = require('path');
+
 /** @type {import('jest-runner-vscode').RunnerOptions} */
 const config = {
 	version: '1.56.2',
 	launchArgs: ['--disable-extensions'],
 	openInFolder: true,
-	extensionDevelopmentPath: __dirname,
+	workspaceDir: path.join(__dirname, 'workspace/workspace.code-workspace'),
+	extensionDevelopmentPath: path.join(__dirname, '../..'),
 };
 
 module.exports = config;
