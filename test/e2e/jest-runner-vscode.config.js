987daf7794ad227819a7fbc2ad5d85c989b2f8e2
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-02-14 00:52:13 +0900

    Bump required VSCode version to 1.86.1
    VSCode 1.86.1 included the Electron 27 update. This comes with Node.js 18.17.1.
    Ref https://code.visualstudio.com/updates/v1_86
    
    Also, this change removes the Node.js version from the CI testing matrix,
    following the `engines.node` field in `package.json`.

--- test/e2e/jest-runner-vscode.config.js
+++ test/e2e/jest-runner-vscode.config.js
@@ -2,9 +2,24 @@
 
 const path = require('path');
 
+const pkg = require('../../package.json');
+
+const vscodeVersion = '1.86.1';
+const requiredVscodeVersion = pkg.engines.vscode.match(/\d+\.\d+\.\d+/)?.[0];
+
+if (!requiredVscodeVersion) {
+	throw new Error('Cannot find a VSCode version in package.json');
+}
+
+if (vscodeVersion !== requiredVscodeVersion) {
+	throw new Error(
+		`The VSCode version '${requiredVscodeVersion}' is required in package.json, but actually '${vscodeVersion}'`,
+	);
+}
+
 /** @type {import('jest-runner-vscode').RunnerOptions} */
 const config = {
-	version: '1.56.2',
+	version: vscodeVersion,
 	launchArgs: ['--disable-extensions'],
 	openInFolder: true,
 	workspaceDir: path.join(__dirname, 'workspace/workspace.code-workspace'),
