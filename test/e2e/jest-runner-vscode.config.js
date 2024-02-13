08524073894015d75f497049ee5d91d081fe513a
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-02-14 08:55:02 +0900

    Bump required VSCode version to 1.82.3 (#503)

--- test/e2e/jest-runner-vscode.config.js
+++ test/e2e/jest-runner-vscode.config.js
@@ -1,10 +1,26 @@
 'use strict';
 
-const path = require('path');
+const path = require('node:path');
+const { readFileSync } = require('node:fs');
+
+const vscodeVersion = '1.82.3';
+
+const pkg = JSON.parse(readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
+const requiredVscodeVersion = pkg.engines.vscode.match(/\d+\.\d+\.\d+/)?.[0];
+
+if (!requiredVscodeVersion) {
+	throw new Error('Cannot find a VSCode version in package.json');
+}
+
+if (vscodeVersion !== requiredVscodeVersion) {
+	throw new Error(
+		`The required VSCode version in package.json is '${requiredVscodeVersion}', but actually '${vscodeVersion}'`,
+	);
+}
 
 /** @type {import('jest-runner-vscode').RunnerOptions} */
 const config = {
-	version: '1.56.2',
+	version: vscodeVersion,
 	launchArgs: ['--disable-extensions'],
 	openInFolder: true,
 	workspaceDir: path.join(__dirname, 'workspace/workspace.code-workspace'),
