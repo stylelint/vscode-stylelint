e8df5d7825b6fe5fca076c9c31a164766683c574
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-02-15 03:38:18 +0900

    Debug more

--- scripts/download-vscode.ts
+++ scripts/download-vscode.ts
@@ -1,4 +1,6 @@
 import { downloadAndUnzipVSCode } from '@vscode/test-electron';
 import { version, platform } from '../test/e2e/jest-runner-vscode.config';
 
-void downloadAndUnzipVSCode(version, platform);
+downloadAndUnzipVSCode(version, platform).then((executablePath) => {
+	console.log(executablePath);
+});
