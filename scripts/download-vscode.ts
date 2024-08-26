8075270dd1712208436c05002a4fadb2e0f6403f
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-08-27 00:43:38 +0900

    Remove no longer needed `scripts/download-vscode.ts`

--- scripts/download-vscode.ts
+++
@@ -1,4 +0,0 @@
-import { downloadAndUnzipVSCode } from '@vscode/test-electron';
-import { version, platform } from '../test/e2e/jest-runner-vscode.config.js';
-
-void downloadAndUnzipVSCode(version, platform);
