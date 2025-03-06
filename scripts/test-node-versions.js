77f387ff4e33e6997a5ac3a60bf09a5196df4e24
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-03-07 02:48:28 +0900

    Refactor `scripts/test-node-versions.js`

--- scripts/test-node-versions.js
+++ scripts/test-node-versions.js
@@ -1,13 +1,10 @@
 'use strict';
 
 const assert = require('node:assert');
-const fs = require('node:fs');
-const path = require('node:path');
 const process = require('node:process');
 
-const packageJsonPath = fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8');
-const packageJson = JSON.parse(packageJsonPath);
+const packageJson = require('../package.json');
 
 const nodeVersion = process.version.slice(1); // trim the 'v' from the version
 
-assert.equal(packageJson.volta.node, nodeVersion, `Invalid "volta.node" in package.json`);
+assert.equal(packageJson.volta.node, nodeVersion, 'Invalid "volta.node" in package.json');
