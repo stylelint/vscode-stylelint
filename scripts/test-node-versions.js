e0c12aa216f30d7ea00bef8befe5b19c9066baef
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-03-07 02:44:43 +0900

    Run different CI from `lint` for Node.js versions

--- scripts/test-node-versions.js
+++ scripts/test-node-versions.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const assert = require('node:assert');
 const fs = require('node:fs');
 const path = require('node:path');
 const process = require('node:process');
@@ -8,11 +9,5 @@ const packageJsonPath = fs.readFileSync(path.join(__dirname, '..', 'package.json
 const packageJson = JSON.parse(packageJsonPath);
 
 const nodeVersion = process.version.slice(1); // trim the 'v' from the version
-const voltaNodeVersion = packageJson.volta.node;
 
-if (nodeVersion !== voltaNodeVersion) {
-	process.stderr.write(
-		`Error: "volta.node" in package.json must be "${nodeVersion}" but is "${voltaNodeVersion}"\n`,
-	);
-	process.exit(1); // eslint-disable-line n/no-process-exit
-}
+assert.equal(packageJson.volta.node, nodeVersion, `Invalid "volta.node" in package.json`);
