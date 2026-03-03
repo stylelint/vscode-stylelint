d706c11e38904ebfed0688cf43890a6aa8283092
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-03-03 23:43:45 +0900

    Suppress ESLint false positive for `no-useless-assignment` rule

--- scripts/launch-vscode.js
+++ scripts/launch-vscode.js
@@ -37,6 +37,7 @@ function parseLaunchArguments(argv) {
 		}
 
 		if (arg === '--') {
+			// eslint-disable-next-line no-useless-assignment -- This variable is actually used in the previous `if` block.
 			passthroughMode = true;
 			passthroughArgs.push(...argv.slice(index));
 			break;
