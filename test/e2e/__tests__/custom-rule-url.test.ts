cf8d464fa094d714a159fe4f80d462f951c75878
Adaline Simonian <adalinesimonian@pm.me>
2026-03-05 20:09:29 +0100

    test: gate stylelint version for e2e test

--- test/e2e/__tests__/custom-rule-url.test.ts
+++ test/e2e/__tests__/custom-rule-url.test.ts
@@ -3,6 +3,7 @@ import {
 	closeAllEditors,
 	waitForDiagnostics,
 	assertDiagnostics,
+	itOnVersion,
 } from '../helpers.js';
 
 describe('Custom rule URL in configuration', () => {
@@ -10,18 +11,22 @@ describe('Custom rule URL in configuration', () => {
 		await closeAllEditors();
 	});
 
-	it('should use custom URL from rule configuration as codeDescription', async () => {
-		const document = await openDocument('custom-rule-url/test.css');
-		const diagnostics = await waitForDiagnostics(document);
+	itOnVersion(
+		'>=16.7',
+		'should use custom URL from rule configuration as codeDescription',
+		async () => {
+			const document = await openDocument('custom-rule-url/test.css');
+			const diagnostics = await waitForDiagnostics(document);
 
-		assertDiagnostics(diagnostics, [
-			{
-				code: 'color-hex-length',
-				codeDescription: 'https://example.com/custom-color-hex-docs',
-				message: 'Expected "#fff" to be "#ffffff" (color-hex-length)',
-				range: [2, 9, 2, 13],
-				severity: 'error',
-			},
-		]);
-	});
+			assertDiagnostics(diagnostics, [
+				{
+					code: 'color-hex-length',
+					codeDescription: 'https://example.com/custom-color-hex-docs',
+					message: 'Expected "#fff" to be "#ffffff" (color-hex-length)',
+					range: [2, 9, 2, 13],
+					severity: 'error',
+				},
+			]);
+		},
+	);
 });
